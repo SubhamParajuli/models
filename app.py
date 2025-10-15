86d08fab689ece40db8e7ab24b1800d1119f5d16
Subham Parajuli <suvamparajuli222@gmail.com>
2025-10-15 10:12:29 +0545

    exception handling

---
+++ app.py
@@ -0,0 +1,11 @@
+from src.mlproject.app_logger import logging
+from src.mlproject.exception import CustomException
+import sys
+
+if __name__== "__main__":
+    logging.info('exectution has started')
+
+    try:
+        a=1/0
+    except Exception as e:
+        raise CustomException(e,sys)
