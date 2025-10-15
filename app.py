2472fc4593fb53eb66e1c624a500fffece01810c
Subham Parajuli <suvamparajuli222@gmail.com>
2025-10-15 12:08:10 +0545

    finished connecting with Mysql

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from src.mlproject.app_logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+
 import sys
 
 if __name__== "__main__":
     logging.info('exectution has started')
 
     try:
-        a=1/0
+        data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         raise CustomException(e,sys)
---
+++ src/mlproject/utils.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.app_logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+
+load_dotenv()
+
+host=os.getenv('host')
+user=os.getenv('user')
+password=os.getenv('password')
+db=os.getenv('db')
+
+def read_sql_data():
+    logging.info('Reading SQL database started')
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection established")
+        df=pd.read_sql_query('Select * from students',mydb)
+        return df
+    except Exception as e:
+        raise CustomException(e)
